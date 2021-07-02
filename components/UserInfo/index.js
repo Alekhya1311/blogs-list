40004fd4d137f3c0db3c30b696dbf516a6843b4f
root <alekhyabairaboina1311@gmail.com>
2021-07-02 20:25:51 +0000

    first commit

---
+++ components/UserInfo/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info">
+    <img
+      className="image"
+      alt="profile pic"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+    />
+    <h1 className="head">Wade Warren</h1>
+    <p className="para">Software developer</p>
+  </div>
+)
+
+export default UserInfo
